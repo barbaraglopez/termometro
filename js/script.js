afce40067ec00e25825f9d1d622ef63a8350b859
Barbara <barbaralopezbbbb@gmail.com>
2021-11-08 10:18:55 -0300

    ajustes en el css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let btnEnviar = document.querySelector("button");
+let input = document.querySelector("input");
+
+btnEnviar.addEventListener("click", function(){
+    if(isNaN(input.value)){
+        alert("Ingresá un valor válido")
+        input.value = ""
+    }else{
+        let resultado = (input.value * 9/5) + 32;
+        input.value = resultado;
+    }
+});
